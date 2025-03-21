e8013d014b6d614b255e08d5a299100bcb1a8095
Daniel Simoes <danielsimoes@daniels-mbp.home>
2025-03-21 01:30:26 +0000

    CustomDrawer Animation

--- app/(drawer)/(tabs)/_layout.tsx
+++ app/(drawer)/(tabs)/_layout.tsx
@@ -3,8 +3,10 @@ import { TouchableOpacity, Text, Image, View, StyleSheet } from 'react-native';
 import { router, Tabs } from 'expo-router';
 import { DrawerToggleButton } from '@react-navigation/drawer';
 import { useNavigation } from '@react-navigation/native';
-import { LinearGradient } from 'expo-linear-gradient'; // Gradiente
-import { Ionicons } from '@expo/vector-icons'; // Ícones do Expo
+import { LinearGradient } from 'expo-linear-gradient';
+import { Ionicons } from '@expo/vector-icons';
+import { useDrawerStatus } from '@react-navigation/drawer'; // Detectar se o Drawer está aberto
+import Animated, { useAnimatedStyle, useSharedValue, withTiming } from 'react-native-reanimated';
 
 import { HapticTab } from '@/components/HapticTab';
 import { IconSymbol } from '@/components/ui/IconSymbol';
@@ -15,123 +17,136 @@ import { useColorScheme } from '@/hooks/useColorScheme';
 export default function TabLayout() {
   const colorScheme = useColorScheme();
   const navigation = useNavigation();
+  const drawerStatus = useDrawerStatus(); // Verifica se o Drawer está aberto
+
+  const scale = useSharedValue(1); // Valor inicial da escala
+  const borderRadius = useSharedValue(0); // Valor inicial da borda
+
+  // Animação da tela quando o Drawer é aberto
+  React.useEffect(() => {
+    if (drawerStatus === 'open') {
+      scale.value = withTiming(0.80, { duration: 200 }); // Reduz o tamanho suavemente
+      borderRadius.value = withTiming(40, { duration: 200 }); // Adiciona bordas arredondadas
+    } else {
+      scale.value = withTiming(1, { duration: 200 }); // Retorna ao normal
+      borderRadius.value = withTiming(0, { duration: 200 });
+    }
+  }, [drawerStatus]);
+
+  const animatedStyle = useAnimatedStyle(() => ({
+    transform: [{ scale: scale.value }],
+    borderRadius: borderRadius.value,
+    overflow: 'hidden',
+  }));
 
   // URL da imagem do perfil
   const randomImageURL = 'https://img.freepik.com/fotos-premium/retrato-de-engenheiro-e-tecnico-de-homem-feliz-na-inspecao-do-painel-de-controle-e-planejamento-de-manutencao-na-area-de-transferencia-sorriso-de-eletricista-masculino-na-subestacao-eletrica-para-sistema-de-energia-e-lista-de-verificacao_590464-170730.jpg';
 
   return (
-    <>
-      <Tabs
-        screenOptions={{
-          tabBarActiveTintColor: '#FF9900', 
-          tabBarInactiveTintColor: '#D3D3D3', 
-          tabBarStyle: {
-            backgroundColor: '#4A4A4A', 
-            borderTopWidth: 0, 
-            paddingTop: 10, 
-            height: 60, // Ajustando altura para acomodar o botão flutuante
-            position: 'absolute',
-            bottom: 0,
-            left: 0,
-            right: 0,
-          },
-          headerShown: true,
-          headerTitle: "", 
-          tabBarButton: HapticTab,
-          tabBarBackground: TabBarBackground,
-          headerLeft: () => (
-            // Mostrar o menu hamburguer apenas nas outras abas
-            <TouchableOpacity
-              style={styles.circleButton} 
-            >
-              {/* <Text style={{ fontSize: 24, color: '#ffffff' }}>☰</Text> */}
-              <DrawerToggleButton tintColor="white"/>
-            </TouchableOpacity>
-          ),
-          headerStyle: {
-            height: 100, 
-            backgroundColor: 'transparent',
-          },
-          headerBackground: () => (
-            <LinearGradient
-              colors={['#995C00', '#FF9900']} 
-              style={{ flex: 1 }}
-            />
-          ),
-          headerRight: () => (
-            // Mostrar o perfil apenas nas outras abas
-            <TouchableOpacity style={{ marginRight: 15 }}>
-              <View style={styles.profileImageContainer}>
-                <Image
-                  source={{ uri: randomImageURL }} 
-                  style={styles.profileImage}
-                />
-              </View>
-            </TouchableOpacity>
-          ),
-        }}
-      >
-        <Tabs.Screen
-          name="index"
-          options={{
-            title: '', 
-            tabBarIcon: ({ color }) => (
-              <Ionicons name="home" size={28} color={color} />
+    <View style={styles.drawerBackground}> 
+    
+      {/* View para garantir fundo branco no Drawer */}
+      <Animated.View style={[styles.animatedContainer, animatedStyle]}>
+        <Tabs
+          screenOptions={{
+            // sceneContainerStyle: { backgroundColor: 'white' }, // Fundo branco para Tabs
+            // drawerContentStyle: { backgroundColor: 'white' }, // Fundo branco para Drawer
+            tabBarActiveTintColor: '#FF9900',
+            tabBarInactiveTintColor: '#D3D3D3',
+            tabBarStyle: {
+              backgroundColor: '#4A4A4A',
+              borderTopWidth: 0,
+              paddingTop: 10,
+              height: 60,
+              position: 'absolute',
+              bottom: 0,
+              left: 0,
+              right: 0,
+            },
+            headerShown: true,
+            headerTitle: "",
+            tabBarButton: HapticTab,
+            tabBarBackground: TabBarBackground,
+            headerLeft: () => (
+              <TouchableOpacity style={styles.circleButton}>
+                <DrawerToggleButton tintColor="white" />
+              </TouchableOpacity>
             ),
-          }}
-        />
-        
-        <Tabs.Screen
-          name="notifications"
-          options={{
-            title: '', 
-            tabBarIcon: ({ color }) => (
-              <Ionicons name="notifications" size={28} color={color} />
-            ),
-            headerShown: true, // Mostrar o header
-            headerTitleAlign: 'center', // Centraliza o título no header
             headerStyle: {
-              backgroundColor: '#FF9900', // Cor do fundo do header
-              height: 100, // Tamanho do header
+              height: 100,
+              backgroundColor: 'transparent',
             },
             headerBackground: () => (
-              <LinearGradient
-                colors={['#995C00', '#FF9900']} 
-                style={{ flex: 1 }}
-              />
+              <LinearGradient colors={['#995C00', '#FF9900']} style={{ flex: 1 }} />
             ),
-            // Mostrar "Notificações" apenas quando na aba "explore"
-            headerTitle: 'Notificações', // Nome do título
-            // Remover os ícones do menu hamburguer e perfil
-            headerLeft: () => null,
-            headerRight: () => null,
+            headerRight: () => (
+              <TouchableOpacity style={{ marginRight: 15 }}>
+                <View style={styles.profileImageContainer}>
+                  <Image source={{ uri: randomImageURL }} style={styles.profileImage} />
+                </View>
+              </TouchableOpacity>
+            ),
           }}
-        />
-      </Tabs>
+        >
+          <Tabs.Screen
+            name="index"
+            options={{
+              title: '',
+              tabBarIcon: ({ color }) => <Ionicons name="home" size={28} color={color} />,
+            }}
+          />
 
-      {/* Botão Flutuante QR Code */}
-      <TouchableOpacity 
-        style={styles.qrButton} 
-        onPress={() => router.push('/camera')} // Agora usa expo-router corretamente
-      >
-        <LinearGradient colors={['#995C00', '#FF9900']} style={styles.qrButtonGradient}>
-          <Ionicons name="qr-code" size={30} color="white" />
-        </LinearGradient>
-      </TouchableOpacity>
-    </>
+          <Tabs.Screen
+            name="notifications"
+            options={{
+              title: '',
+              tabBarIcon: ({ color }) => <Ionicons name="notifications" size={28} color={color} />,
+              headerShown: true,
+              headerTitleAlign: 'center',
+              headerStyle: {
+                backgroundColor: '#FF9900',
+                height: 100,
+              },
+              headerBackground: () => (
+                <LinearGradient colors={['#995C00', '#FF9900']} style={{ flex: 1 }} />
+              ),
+              headerTitle: 'Notificações',
+              headerLeft: () => null,
+              headerRight: () => null,
+            }}
+          />
+        </Tabs>
+
+        {/* Botão Flutuante QR Code */}
+        <TouchableOpacity style={styles.qrButton} onPress={() => router.push('/camera')}>
+          <LinearGradient colors={['#995C00', '#FF9900']} style={styles.qrButtonGradient}>
+            <Ionicons name="qr-code" size={30} color="white" />
+          </LinearGradient>
+        </TouchableOpacity>
+      </Animated.View>
+      
+    </View>
   );
 }
 
 const styles = StyleSheet.create({
+  drawerBackground: {
+    flex: 1,
+    backgroundColor: '#2F2F2F', // Garante que o fundo do Drawer fique branco
+  },
+  animatedContainer: {
+    flex: 1,
+    backgroundColor: 'white', // Mantém o fundo branco mesmo com a animação
+  },
   circleButton: {
     marginLeft: 15,
     width: 40,
     height: 40,
-    borderRadius: 20, 
-    backgroundColor: '#ffffff56', 
+    borderRadius: 20,
+    backgroundColor: '#ffffff56',
     justifyContent: 'center',
     alignItems: 'center',
-    elevation: 5, 
+    elevation: 5,
   },
   profileImageContainer: {
     width: 40,
@@ -148,15 +163,15 @@ const styles = StyleSheet.create({
   },
   qrButton: {
     position: 'absolute',
-    bottom: 30, 
+    bottom: 30,
     left: '50%',
-    marginLeft: -35, // Metade da largura para centralizar
+    marginLeft: -35,
     width: 70,
     height: 70,
     borderRadius: 35,
     justifyContent: 'center',
     alignItems: 'center',
-    elevation: 10, 
+    elevation: 10,
   },
   qrButtonGradient: {
     width: '100%',
--- app/(drawer)/_layout.tsx
+++ app/(drawer)/_layout.tsx
@@ -1,29 +1,217 @@
 import { Drawer } from 'expo-router/drawer';
 import { Ionicons } from '@expo/vector-icons';
-
+import { StatusBar } from 'expo-status-bar';
+import { useNavigationState } from '@react-navigation/native';
+import { useEffect, useState } from 'react';
+import { DrawerContentScrollView, DrawerItem } from '@react-navigation/drawer';
+import { processColorsInProps } from 'react-native-reanimated/lib/typescript/Colors';
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 export default function DrawerLayout() {
+  const navigationState = useNavigationState((state) => state);
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+  // 🔥 Detecta se o Drawer está aberto e atualiza o estado
+  useEffect(() => {
+    if (navigationState?.routes) {
+      const drawerRoute = navigationState.routes.find((route) => route.name === "(drawer)");
+      // setIsDrawerOpen(drawerRoute?.state?.index > 0);
+    }
+  }, [navigationState]);
+// URL da imagem do perfil
+const randomImageURL = 'https://img.freepik.com/fotos-premium/retrato-de-engenheiro-e-tecnico-de-homem-feliz-na-inspecao-do-painel-de-controle-e-planejamento-de-manutencao-na-area-de-transferencia-sorriso-de-eletricista-masculino-na-subestacao-eletrica-para-sistema-de-energia-e-lista-de-verificacao_590464-170730.jpg';
+
+  const CustomDrawerContent = (props) => {
+    return(
+      <>
+        <StatusBar style={isDrawerOpen ? 'dark' : 'light'} />
+        <DrawerContentScrollView {...props}>
+        <View style={styles.drawerContainer}>
+      {/* Perfil do Usuário */}
+      <View style={styles.profileContainer}>
+        <Image
+          source={{ uri: randomImageURL }} style={styles.profileImage}
+          style={styles.profileImage}
+        />
+        <Text style={styles.profileName}>Mark Philips</Text>
+        <Text style={styles.profileEmail}>markphil@gmail.com</Text>
+      </View>
+
+      {/* Opções do Menu */}
+      <TouchableOpacity style={styles.menuItem} onPress={() => {}}>
+        <Ionicons name="settings-outline" size={24} color="white" />
+        <Text style={styles.menuText}>Configurações</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.menuItem} onPress={() => {}}>
+        <Ionicons name="headset-outline" size={24} color="white" />
+        <Text style={styles.menuText}>Suporte</Text>
+      </TouchableOpacity>
+
+      <TouchableOpacity style={styles.menuItem} onPress={() => {}}>
+        <Ionicons name="information-circle-outline" size={24} color="white" />
+        <Text style={styles.menuText}>CornerStone</Text>
+      </TouchableOpacity>
+
+      {/* Botão de Logout */}
+      <View style={styles.logoutContainer}>
+        <TouchableOpacity style={styles.logoutButton} onPress={() => {}}>
+          <Ionicons name="log-out-outline" size={24} color="white" />
+          <Text style={styles.logoutText}>Logout</Text>
+        </TouchableOpacity>
+        <Text style={styles.versionText}>BeiraMar v1.0.0</Text>
+      </View>
+    </View>
+        </DrawerContentScrollView>
+      </>
+    )
+  }
+
   return (
-    <Drawer
-      screenOptions={{
-        headerShown: false, // 🔥 Esconde o header duplicado do Drawer
-        drawerActiveTintColor: '#FF9900',
-        drawerInactiveTintColor: '#aaa',
-      }}
-    >
-      <Drawer.Screen
-        name="(tabs)"
-        options={{
-          title: 'Início',
-          drawerIcon: ({ color }) => <Ionicons name="home" size={24} color={color} />,
+    <>
+      {/* 🔥 Muda a cor da StatusBar dinamicamente */}
+      <StatusBar style={isDrawerOpen ? 'dark' : 'light'} />
+      <Drawer
+        drawerContent={(props) => <CustomDrawerContent {...props} />}
+        screenOptions={{
+          headerShown: false,
+          overlayColor: 'transparent',
+          drawerStyle: {
+            backgroundColor: '#2F2F2F', // 🔥 Fundo branco do Drawer
+            width: 200,
+          },
         }}
       />
-      <Drawer.Screen
-        name="profile"
-        options={{
-          title: 'Perfil',
-          drawerIcon: ({ color }) => <Ionicons name="person" size={24} color={color} />,
-        }}
-      />
-    </Drawer>
+    </>
   );
 }
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: "bold",
+    color: "white",
+  },
+  drawerContainer: {
+    flex: 1,
+    paddingHorizontal: 20,
+  },
+  profileContainer: {
+
+    marginBottom: 30,
+  },
+  profileImage: {
+    width: 70,
+    height: 70,
+    borderRadius: 35,
+    borderWidth: 1,
+    borderColor: "white",
+    
+  },
+  profileName: {
+    color: "white",
+    fontSize: 18,
+    fontWeight: "bold",
+    marginTop: 10,
+  },
+  profileEmail: {
+    fontSize: 14,
+    color: "white",
+  },
+  menuItem: {
+    flexDirection: "row",
+    alignItems: "center",
+    paddingVertical: 12,
+  },
+  menuText: {
+    fontSize: 16,
+    marginLeft: 15,
+    color: "white",
+  },
+  logoutContainer: {
+    marginTop: 390,
+
+  
+  },
+  logoutButton: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  logoutText: {
+    fontSize: 16,
+    marginLeft: 10,
+    color: "white",
+  },
+  versionText: {
+    fontSize: 12,
+    marginTop: 10,
+    color: "white",
+  },
+});
+
+
+
+
+// import { Drawer } from 'expo-router/drawer';
+// import { Ionicons } from '@expo/vector-icons';
+// import { StatusBar } from 'expo-status-bar';
+// import { useNavigationState } from '@react-navigation/native';
+// import { useEffect, useState } from 'react';
+// import { processColorsInProps } from 'react-native-reanimated/lib/typescript/Colors';
+
+// export default function DrawerLayout() {
+//   const navigationState = useNavigationState((state) => state);
+//   const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+
+//   // 🔥 Detecta se o Drawer está aberto e atualiza o estado
+//   useEffect(() => {
+//     if (navigationState?.routes) {
+//       const drawerRoute = navigationState.routes.find((route) => route.name === "(drawer)");
+//       // setIsDrawerOpen(drawerRoute?.state?.index > 0);
+//     }
+//   }, [navigationState]);
+
+//   return (
+//     <>
+//       {/* 🔥 Muda a cor da StatusBar dinamicamente */}
+//       <StatusBar style={isDrawerOpen ? 'dark' : 'light'} />
+
+//       <Drawer
+//         screenOptions={{
+//           headerShown: false,
+//           overlayColor: 'transparent',
+//           drawerActiveTintColor: '#FF9900',
+//           drawerInactiveTintColor: '#aaa',
+//           drawerStyle: {
+//             backgroundColor: '#FFFFFF', // 🔥 Fundo branco do Drawer
+//             width: 260,
+//           },
+//           drawerLabelStyle: {
+//             fontSize: 16,
+//             color: '#333',
+//           },
+//         }}
+//       >
+//         <Drawer.Screen
+//           name="(tabs)"
+//           options={{
+//             title: 'Configurações',
+//             drawerIcon: ({ color }) => <Ionicons name="settings" size={20} color={color} />,
+//           }}
+//         />
+//          <Drawer.Screen
+//           name="index"
+//           options={{
+//             title: 'Suport',
+//             drawerIcon: ({ color }) => <Ionicons name="call" size={20} color={color} />,
+//           }}
+//         />
+//       </Drawer>
+//     </>
+//   );
+// }
